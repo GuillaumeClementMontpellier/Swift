44c693b35d0b3d71c3052c421dda67a558e3c4f6
user164554 <user164554@92-63-141-243.rdns.melbourne.co.uk>
2020-02-28 13:36:43 +0000

    init

---
+++ MobileSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MobileSwift
+//
+//  Created by user164554 on 2/28/20.
+//  Copyright © 2020 com.pinou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
